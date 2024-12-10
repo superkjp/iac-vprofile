896a71a13e4d8596db2463b0c5ae2ec54d17d001
superkjp <kangjinpeng513@gmail.com>
2024-12-10 19:30:57 +1100

    fix

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -11,15 +11,8 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
-# resource "aws_eks_node_group" "managed_node_group1"{
-#   cluster_name = local.cluster_name
-  
-
-
-# }
   eks_managed_node_groups= {
     one = {
       name = "node-group-1"
